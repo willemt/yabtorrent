9bb352760a2268c8920abea303e96a2e6da5a20f
willem <himself@willemthiart.com>
2013-06-23 00:48:39 +1200

    added better win32 def flag

--- bt_util.h
+++ bt_util.h
@@ -1,5 +1,9 @@
 
 char *url2host(const char *url);
-char* strndup(const char* str, const unsigned int len);
 
 char *bt_generate_peer_id();
+
+#if WIN32
+char* strndup(const char* str, const unsigned int len);
+#endif
+
